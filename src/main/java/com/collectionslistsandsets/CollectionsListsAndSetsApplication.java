11f4782f6ec36db751e1bcf6ce09168cc457ef93
Nikita-Sychev <komap3965@icloud.com>
2023-07-18 23:58:39 +0300

    Initial commit

---
+++ src/main/java/com/collectionslistsandsets/CollectionsListsAndSetsApplication.java
@@ -0,0 +1,13 @@
+package com.collectionslistsandsets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsListsAndSetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionsListsAndSetsApplication.class, args);
+    }
+
+}
