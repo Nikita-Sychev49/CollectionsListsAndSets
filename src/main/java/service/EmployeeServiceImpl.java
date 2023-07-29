142dc14cf9979278d00ab94fff95f7d1029dc61d
Nikita-Sychev <komap3965@icloud.com>
2023-07-29 21:35:01 +0300

    Вторая часть с Map

--- src/main/java/service/EmployeeServiceImpl.java
+++ src/main/java/service/EmployeeServiceImpl.java
@@ -5,37 +5,30 @@ import exeption.EmployeeNotFoundException;
 import model.Employee;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    private final List<Employee> employeeList;
+    private final Map<String, Employee> employeeMap;
 
-    public EmployeeServiceImpl(List<Employee> employeeList) {
-        this.employeeList = new ArrayList<>();
+    public EmployeeServiceImpl(List<Employee> employeeMap) {
+        this.employeeMap = new HashMap<>();
     }
 
-
     @Override
-
     public Employee add(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employeeList.contains(employee)) {
+        if (employeeMap.containsKey(employee.getFullName())) {
             throw new EmployeeAlreadyAddedException("Такой сотрудник уже есть");
         }
-        employeeList.add(employee);
-        return employee;
+        return employeeMap.put(employee.getFullName(), employee);
     }
 
     @Override
     public Employee remove(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employeeList.contains(employee)) {
-            employeeList.remove(employee);
-            return employee;
+        if (employeeMap.containsKey(employee.getFullName())) {
+            return employeeMap.remove(employee);
         }
         throw new EmployeeNotFoundException("Сотрудник не найден");
     }
@@ -43,14 +36,14 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public Employee find(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employeeList.contains(employee)) {
-            return employee;
+        if (employeeMap.containsKey(employee.getFullName())) {
+            return employeeMap.get(employee.getFullName());
         }
         throw new EmployeeNotFoundException("Сотрудник не найден");
     }
 
     @Override
     public Collection<Employee> findAll() {
-        return Collections.unmodifiableList(employeeList);
+        return employeeMap.values();
     }
 }
